23ee069a1acd29a4313fa85b2103a17bc77353e7
jesse318s <jesse318s@gmail.com>
2024-07-25 16:45:47 -0500

    Add project files.

---
+++ practice-project-with-wpf-csharp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace practice_project_with_wpf_csharp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
